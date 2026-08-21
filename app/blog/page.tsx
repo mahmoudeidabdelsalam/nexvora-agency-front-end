262e7aaa36eefe54ba39fceadca732deef1828ec
Mahmoud Eid <mahmoudeidabdelsalam89@gmail.com>
2026-08-21 05:09:27 +0300

    fix actions

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,8 +17,8 @@ export default async function BlogPage() {
           <div className="mt-10 grid gap-4 md:grid-cols-2">
             {posts.map((post) => (
               <article key={post.slug} className="rounded-md border border-[#262263]/10 bg-[#f7fbfc] p-2 shadow-sm">
-                <img className="h-120 w-full object-cover mb-5" src={post?.featuredImage?.node?.sourceUrl } alt={post.title} />
-                <h2 className="text-xl font-semibold text-[#262263]">{post.title}</h2>
+                <img className="h-70 w-full object-cover mb-5" src={post?.featuredImage?.node?.sourceUrl } alt={post.title} />
+                <Link href={`/blog/${post.slug}`} className="text-xl font-semibold text-[#262263]">{post.title}</Link>
                 <div
                   className="mt-3 text-sm leading-7 text-slate-600"
                   dangerouslySetInnerHTML={{
