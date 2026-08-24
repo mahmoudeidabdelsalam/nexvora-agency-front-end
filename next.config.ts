ac816e00f8325303b64b97856d0c921c3fa398bc
Mahmoud Eid <mahmoudeidabdelsalam89@gmail.com>
2026-08-24 16:48:27 +0300

    add link to header

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
   trailingSlash: true,
+
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Link",
+            value: '</service/>; rel="service-doc", </about-us/>; rel="about"',
+          },
+        ],
+      },
+    ];
+  },
 
   images: {
     unoptimized: true,
